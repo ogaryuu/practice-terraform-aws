654741c6871623e01cfacd623c0bfa3d900500bf
ogaryuu <ogaryuuprogramming@gmail.com>
2025-10-14 07:51:20 +0900

    add: aws vpc settings
    Add vpc settings to network.tf

--- .terraform/network.tf
+++ .terraform/network.tf
@@ -1,1 +1,16 @@
+# ----------------------------------
+# VPC
+# ----------------------------------
+resource "aws_vpc" "vpc" {
+  cidr_block           = "192.168.0.0/24"
+  instance_tenancy     = "default"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  assign_generated_ipv6_cidr_block = false
 
+  tags = {
+    Name        = "${var.project}-${var.environment}-vpc"
+    Project     = var.project
+    Environment = var.environment
+  }
+}
