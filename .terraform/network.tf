488b3ed173298bac078a04e628c3ed75b9aa8b24
ogaryuu <ogaryuuprogramming@gmail.com>
2025-10-14 10:58:52 +0900

    add: aws vpc subnets settings
    Add vpc subnets  settings to network.tf

--- .terraform/network.tf
+++ .terraform/network.tf
@@ -2,15 +2,74 @@
 # VPC
 # ----------------------------------
 resource "aws_vpc" "vpc" {
-  cidr_block           = "192.168.0.0/24"
-  instance_tenancy     = "default"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
+  cidr_block                       = "192.168.0.0/24"
+  instance_tenancy                 = "default"
+  enable_dns_support               = true
+  enable_dns_hostnames             = true
   assign_generated_ipv6_cidr_block = false
 
   tags = {
     Name        = "${var.project}-${var.environment}-vpc"
     Project     = var.project
     Environment = var.environment
+  }
+}
+
+# ----------------------------------
+# Subnets
+# ----------------------------------
+resource "aws_subnet" "public_subnet_1a" {
+  vpc_id                  = aws_vpc.vpc.id
+  availability_zone       = "ap-northeast-1a"
+  cidr_block              = "192.168.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name        = "${var.project}-${var.environment}-public-subnet-1a"
+    Project     = var.project
+    Environment = var.environment
+    type        = "public"
+  }
+}
+
+resource "aws_subnet" "public_subnet_1b" {
+  vpc_id                  = aws_vpc.vpc.id
+  availability_zone       = "ap-northeast-1a"
+  cidr_block              = "192.168.2.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name        = "${var.project}-${var.environment}-public-subnet-1b"
+    Project     = var.project
+    Environment = var.environment
+    type        = "public"
+  }
+}
+
+resource "aws_subnet" "public_subnet_1c" {
+  vpc_id                  = aws_vpc.vpc.id
+  availability_zone       = "ap-northeast-1a"
+  cidr_block              = "192.168.3.0/25"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name        = "${var.project}-${var.environment}-private-subnet-1c"
+    Project     = var.project
+    Environment = var.environment
+    type        = "private"
+  }
+}
+
+resource "aws_subnet" "public_subnet_1d" {
+  vpc_id                  = aws_vpc.vpc.id
+  availability_zone       = "ap-northeast-1a"
+  cidr_block              = "192.168.4.0/25"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name        = "${var.project}-${var.environment}-private-subnet-1d"
+    Project     = var.project
+    Environment = var.environment
+    type        = "private"
   }
 }
