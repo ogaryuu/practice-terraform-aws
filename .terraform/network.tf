02b850f5c4f781e3e14ebd575cda3da7f4edab1e
ogaryuu <ogaryuuprogramming@gmail.com>
2025-10-15 08:07:20 +0900

    add: aws internet gateway settings
    Add  internet gateway settings to network.tf

--- .terraform/network.tf
+++ .terraform/network.tf
@@ -116,3 +116,22 @@ resource "aws_route_table_association" "private_rt_assoc_1d" {
   subnet_id      = aws_subnet.public_subnet_1d.id
   route_table_id = aws_route_table.private_rt.id
 }
+
+# ----------------------------------
+# Internet Gateway
+# ----------------------------------
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name        = "${var.project}-${var.environment}-igw"
+    Project     = var.project
+    Environment = var.environment
+  }
+}
+
+resource "aws_route" "public_rt_internet_access" {
+  route_table_id         = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
