bcdd99b0b22f3fcfc597bcab131d7b8466793d84
ogaryuu <ogaryuuprogramming@gmail.com>
2025-10-15 15:57:10 +0900

    add: aws db  security group settings
    Add database  security group  settings to security_group.tf

--- .terraform/security_group.tf
+++ .terraform/security_group.tf
@@ -44,10 +44,7 @@ resource "aws_security_group_rule" "web_out_tcp3000" {
   protocol          = "tcp"
   from_port         = 3000
   to_port           = 3000
-  
-  cidr_blocks = [
-    "0.0.0.0/0"
-  ]
+  source_security_group_id = aws_security_group.app_sg.id
 }
 
 # app server security group
@@ -122,4 +119,26 @@ resource "aws_security_group_rule" "opmng_out_https" {
   cidr_blocks = [
     "0.0.0.0/0"
   ]
+}
+
+# db security group
+resource "aws_security_group" "db_sg" {
+  name        = "${var.project}-${var.environment}-db-sg"
+  description = "database role security group"
+  vpc_id      = aws_vpc.vpc
+
+  tags = {
+    Name        = "${var.project}-${var.environment}-db-sg"
+    Environment = var.environment
+    Project     = var.project
+  }
+}
+
+resource "aws_security_group_rule" "db_in_tcp3306" {
+  security_group_id = aws_security_group.db_sg.id
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 3306
+  to_port           = 3306
+  source_security_group_id = aws_security_group.app_sg.id
 }
