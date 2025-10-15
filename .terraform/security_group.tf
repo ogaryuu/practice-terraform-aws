35dc543521987686353624836d4954b4d0c3f5ac
ogaryuu <ogaryuuprogramming@gmail.com>
2025-10-15 15:49:20 +0900

    add: aws opmng  security group settings
    Add operation and management  security group  settings to security_group.tf

--- .terraform/security_group.tf
+++ .terraform/security_group.tf
@@ -62,3 +62,64 @@ resource "aws_security_group" "app_sg" {
     Project     = var.project
   }
 }
+
+# opmng security group
+resource "aws_security_group" "opmng_sg" {
+  name        = "${var.project}-${var.environment}-opmng-sg"
+  description = "operation and management security group"
+  vpc_id      = aws_vpc.vpc
+
+  tags = {
+    Name        = "${var.project}-${var.environment}-opmng-sg"
+    Environment = var.environment
+    Project     = var.project
+  }
+}
+
+resource "aws_security_group_rule" "opmng_in_ssh" {
+  security_group_id = aws_security_group.opmng_sg.id
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 22
+  to_port           = 22
+  
+  cidr_blocks = [
+    "0.0.0.0/0"
+  ]
+}
+
+resource "aws_security_group_rule" "opmng_in_tcp3000" {
+  security_group_id = aws_security_group.opmng_sg.id
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 3000
+  to_port           = 3000
+  
+  cidr_blocks = [
+    "0.0.0.0/0"
+  ]
+}
+
+resource "aws_security_group_rule" "opmng_out_http" {
+  security_group_id = aws_security_group.example.id
+  type              = "egress"
+  protocol          = "tcp"
+  from_port         = 80
+  to_port           = 80
+  
+  cidr_blocks = [
+    "0.0.0.0/0"
+  ]
+}
+
+resource "aws_security_group_rule" "opmng_out_https" {
+  security_group_id = aws_security_group.example.id
+  type              = "egress"
+  protocol          = "tcp"
+  from_port         = 443
+  to_port           = 443
+  
+  cidr_blocks = [
+    "0.0.0.0/0"
+  ]
+}
