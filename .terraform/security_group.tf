0ec8a16a520f2af3d90045b1c39a60de13510664
ogaryuu <ogaryuuprogramming@gmail.com>
2025-10-15 15:43:09 +0900

    add: aws web server security group settings
    Add web server security group  settings to security_group.tf

---
+++ .terraform/security_group.tf
@@ -0,0 +1,51 @@
+# --------------------------------
+# Security Group
+# --------------------------------
+# web server security group
+resource "aws_security_group" "web_sg" {
+  name        = "${var.project}-${var.environment}-web-sg"
+  description = "web front role security group"
+  vpc_id      = aws_vpc.vpc
+
+  tags = {
+    Name        = "${var.project}-${var.environment}-web-sg"
+    Environment = var.environment
+    Project     = var.project
+  }
+}
+
+resource "aws_security_group_rule" "web_in_http" {
+  security_group_id = aws_security_group.example.id
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 80
+  to_port           = 80
+  
+  cidr_blocks = [
+    "0.0.0.0/0"
+  ]
+}
+
+resource "aws_security_group_rule" "web_in_https" {
+  security_group_id = aws_security_group.example.id
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 443
+  to_port           = 443
+  
+  cidr_blocks = [
+    "0.0.0.0/0"
+  ]
+}
+
+resource "aws_security_group_rule" "web_out_tcp3000" {
+  security_group_id = aws_security_group.example.id
+  type              = "egress"
+  protocol          = "tcp"
+  from_port         = 3000
+  to_port           = 3000
+  
+  cidr_blocks = [
+    "0.0.0.0/0"
+  ]
+}
