6769ce0c27b8fbbc607ab290ec87b70895d28d17
ogaryuu <ogaryuuprogramming@gmail.com>
2025-10-21 13:43:06 +0900

    add: define RDS instance resource and random string for database password

--- .terraform/rds.tf
+++ .terraform/rds.tf
@@ -41,3 +41,11 @@ resource "aws_db_subnet_group" "mysql_standalone_subnet_group" {
     Project     = var.project
   }
 }
+
+# --------------------------------
+# RDS instance
+# --------------------------------
+resource "random_string" "db_password" {
+  length  = 16
+  special = false
+}
