2b0c29fd49cede937372ba6f6b332f9ce8aae194
ogaryuu <ogaryuuprogramming@gmail.com>
2025-11-04 08:09:31 +0900

    remove: comment out network configuration in Terraform

--- .terraform/network.tf
+++ .terraform/network.tf
@@ -1,136 +1,136 @@
-# ----------------------------------
-# VPC
-# ----------------------------------
-resource "aws_vpc" "vpc" {
-  cidr_block                       = "192.168.0.0/20"
-  instance_tenancy                 = "default"
-  enable_dns_support               = true
-  enable_dns_hostnames             = true
-  assign_generated_ipv6_cidr_block = false
+# # ----------------------------------
+# # VPC
+# # ----------------------------------
+# resource "aws_vpc" "vpc" {
+#   cidr_block                       = "192.168.0.0/20"
+#   instance_tenancy                 = "default"
+#   enable_dns_support               = true
+#   enable_dns_hostnames             = true
+#   assign_generated_ipv6_cidr_block = false
 
-  tags = {
-    Name        = "${var.project}-${var.environment}-vpc"
-    Project     = var.project
-    Environment = var.environment
-  }
-}
+#   tags = {
+#     Name        = "${var.project}-${var.environment}-vpc"
+#     Project     = var.project
+#     Environment = var.environment
+#   }
+# }
 
-# ----------------------------------
-# Subnets
-# ----------------------------------
-resource "aws_subnet" "public_subnet_1a" {
-  vpc_id                  = aws_vpc.vpc.id
-  availability_zone       = "ap-northeast-1a"
-  cidr_block              = "192.168.1.0/24"
-  map_public_ip_on_launch = true
+# # ----------------------------------
+# # Subnets
+# # ----------------------------------
+# resource "aws_subnet" "public_subnet_1a" {
+#   vpc_id                  = aws_vpc.vpc.id
+#   availability_zone       = "ap-northeast-1a"
+#   cidr_block              = "192.168.1.0/24"
+#   map_public_ip_on_launch = true
 
-  tags = {
-    Name        = "${var.project}-${var.environment}-public-subnet-1a"
-    Project     = var.project
-    Environment = var.environment
-    type        = "public"
-  }
-}
+#   tags = {
+#     Name        = "${var.project}-${var.environment}-public-subnet-1a"
+#     Project     = var.project
+#     Environment = var.environment
+#     type        = "public"
+#   }
+# }
 
-resource "aws_subnet" "public_subnet_1c" {
-  vpc_id                  = aws_vpc.vpc.id
-  availability_zone       = "ap-northeast-1c"
-  cidr_block              = "192.168.2.0/24"
-  map_public_ip_on_launch = true
-  tags = {
-    Name        = "${var.project}-${var.environment}-public-subnet-1c"
-    Project     = var.project
-    Environment = var.environment
-    type        = "public"
-  }
-}
+# resource "aws_subnet" "public_subnet_1c" {
+#   vpc_id                  = aws_vpc.vpc.id
+#   availability_zone       = "ap-northeast-1c"
+#   cidr_block              = "192.168.2.0/24"
+#   map_public_ip_on_launch = true
+#   tags = {
+#     Name        = "${var.project}-${var.environment}-public-subnet-1c"
+#     Project     = var.project
+#     Environment = var.environment
+#     type        = "public"
+#   }
+# }
 
-resource "aws_subnet" "private_subnet_1a" {
-  vpc_id                  = aws_vpc.vpc.id
-  availability_zone       = "ap-northeast-1a"
-  cidr_block              = "192.168.3.0/24"
-  map_public_ip_on_launch = false
+# resource "aws_subnet" "private_subnet_1a" {
+#   vpc_id                  = aws_vpc.vpc.id
+#   availability_zone       = "ap-northeast-1a"
+#   cidr_block              = "192.168.3.0/24"
+#   map_public_ip_on_launch = false
 
-  tags = {
-    Name        = "${var.project}-${var.environment}-private-subnet-1a"
-    Project     = var.project
-    Environment = var.environment
-    type        = "private"
-  }
-}
+#   tags = {
+#     Name        = "${var.project}-${var.environment}-private-subnet-1a"
+#     Project     = var.project
+#     Environment = var.environment
+#     type        = "private"
+#   }
+# }
 
-resource "aws_subnet" "private_subnet_1c" {
-  vpc_id                  = aws_vpc.vpc.id
-  availability_zone       = "ap-northeast-1c"
-  cidr_block              = "192.168.4.0/24"
-  map_public_ip_on_launch = false
+# resource "aws_subnet" "private_subnet_1c" {
+#   vpc_id                  = aws_vpc.vpc.id
+#   availability_zone       = "ap-northeast-1c"
+#   cidr_block              = "192.168.4.0/24"
+#   map_public_ip_on_launch = false
 
-  tags = {
-    Name        = "${var.project}-${var.environment}-private-subnet-1c"
-    Project     = var.project
-    Environment = var.environment
-    type        = "private"
-  }
-}
+#   tags = {
+#     Name        = "${var.project}-${var.environment}-private-subnet-1c"
+#     Project     = var.project
+#     Environment = var.environment
+#     type        = "private"
+#   }
+# }
 
-# ----------------------------------
-# Route Tables
-# ----------------------------------
-resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.vpc.id
-  tags = {
-    Name        = "${var.project}-${var.environment}-public-rt"
-    Project     = var.project
-    Environment = var.environment
-    type        = "public"
-  }
-}
+# # ----------------------------------
+# # Route Tables
+# # ----------------------------------
+# resource "aws_route_table" "public_rt" {
+#   vpc_id = aws_vpc.vpc.id
+#   tags = {
+#     Name        = "${var.project}-${var.environment}-public-rt"
+#     Project     = var.project
+#     Environment = var.environment
+#     type        = "public"
+#   }
+# }
 
-resource "aws_route_table_association" "public_rt_assoc_1a" {
-  subnet_id      = aws_subnet.public_subnet_1a.id
-  route_table_id = aws_route_table.public_rt.id
-}
+# resource "aws_route_table_association" "public_rt_assoc_1a" {
+#   subnet_id      = aws_subnet.public_subnet_1a.id
+#   route_table_id = aws_route_table.public_rt.id
+# }
 
-resource "aws_route_table_association" "public_rt_assoc_1c" {
-  subnet_id      = aws_subnet.public_subnet_1c.id
-  route_table_id = aws_route_table.public_rt.id
-}
+# resource "aws_route_table_association" "public_rt_assoc_1c" {
+#   subnet_id      = aws_subnet.public_subnet_1c.id
+#   route_table_id = aws_route_table.public_rt.id
+# }
 
-resource "aws_route_table" "private_rt" {
-  vpc_id = aws_vpc.vpc.id
-  tags = {
-    Name        = "${var.project}-${var.environment}-private-rt"
-    Project     = var.project
-    Environment = var.environment
-    type        = "private"
-  }
-}
+# resource "aws_route_table" "private_rt" {
+#   vpc_id = aws_vpc.vpc.id
+#   tags = {
+#     Name        = "${var.project}-${var.environment}-private-rt"
+#     Project     = var.project
+#     Environment = var.environment
+#     type        = "private"
+#   }
+# }
 
-resource "aws_route_table_association" "private_rt_assoc_1a" {
-  subnet_id      = aws_subnet.private_subnet_1a.id
-  route_table_id = aws_route_table.private_rt.id
-}
+# resource "aws_route_table_association" "private_rt_assoc_1a" {
+#   subnet_id      = aws_subnet.private_subnet_1a.id
+#   route_table_id = aws_route_table.private_rt.id
+# }
 
-resource "aws_route_table_association" "private_rt_assoc_1c" {
-  subnet_id      = aws_subnet.private_subnet_1c.id
-  route_table_id = aws_route_table.private_rt.id
-}
+# resource "aws_route_table_association" "private_rt_assoc_1c" {
+#   subnet_id      = aws_subnet.private_subnet_1c.id
+#   route_table_id = aws_route_table.private_rt.id
+# }
 
-# ----------------------------------
-# Internet Gateway
-# ----------------------------------
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.vpc.id
+# # ----------------------------------
+# # Internet Gateway
+# # ----------------------------------
+# resource "aws_internet_gateway" "igw" {
+#   vpc_id = aws_vpc.vpc.id
 
-  tags = {
-    Name        = "${var.project}-${var.environment}-igw"
-    Project     = var.project
-    Environment = var.environment
-  }
-}
+#   tags = {
+#     Name        = "${var.project}-${var.environment}-igw"
+#     Project     = var.project
+#     Environment = var.environment
+#   }
+# }
 
-resource "aws_route" "public_rt_internet_access" {
-  route_table_id         = aws_route_table.public_rt.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.igw.id
-}
+# resource "aws_route" "public_rt_internet_access" {
+#   route_table_id         = aws_route_table.public_rt.id
+#   destination_cidr_block = "0.0.0.0/0"
+#   gateway_id             = aws_internet_gateway.igw.id
+# }
